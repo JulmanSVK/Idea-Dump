bb3d7ea713db94641ef2a119530833f152b23fe1
JulmanSVK <julman.gaming@gmail.com>
2023-06-04 02:10:18 +0200

    System detection + FE Changes

--- ShutdownScript/ShutdownScript.py
+++ ShutdownScript/ShutdownScript.py
@@ -1,15 +1,23 @@
 from tkinter import *
 import tkinter as tk
 import os
+from sys import platform
+
+print(platform)
 
 win = Tk()
-win.geometry("400x200")
+win.geometry("300x175")
 
 radio = IntVar()
 time_var = tk.StringVar()
 
 def abort():
-    os.system("shutdown /a")
+    if platform == "win32":
+        os.system("shutdown /a")
+    elif platform == "linux" or platform == "linux2":
+        os.system("sudo shutdown -c")
+    elif platform == "darwin":
+        os.system()
     print("Pc shutdown aborted")
 
 def main():
@@ -33,12 +41,14 @@ def main():
     else:
         print("Niečo sa podrbalo")
 
+nadpis = tk.Label(win,text="Vypnutie PC",font="bold").pack()
 time_ent = tk.Entry(win,textvariable = time_var).pack()
 
-r1 = Radiobutton(win, text = "Seconds", variable=radio, value=1).pack()
+if platform != "linux" or platform != "linux2":
+    r1 = Radiobutton(win, text = "Seconds", variable=radio, value=1).pack()
 r2 = Radiobutton(win, text = "Minutes", variable=radio, value=2).pack()
 r3 = Radiobutton(win, text = "Hours", variable=radio, value=3).pack()
-
-sub_btn = tk.Button(win,text = 'Submit', command = main).pack()
-stp_btn = tk.Button(win,text = 'Stop Shutdown', command = abort).pack()
+sep = tk.Separator(win)
+sub_btn = tk.Button(win,text = 'Submit', command = main).pack(side=RIGHT,padx=20)
+stp_btn = tk.Button(win,text = 'Stop Shutdown', command = abort, fg="white",bg="red").pack(side=LEFT,padx=20)
 mainloop()
